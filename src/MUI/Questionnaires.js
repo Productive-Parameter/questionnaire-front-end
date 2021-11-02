ae02a3e00ea054305879ba1b8b4737b8a24fa184
Sampsa <sampsaleikas@gmail.com>
2021-11-02 21:36:59 +0200

    näyttää kyssärit nyt forntissa

--- src/MUI/Questionnaires.js
+++ src/MUI/Questionnaires.js
@@ -4,30 +4,69 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
+
 export default function Questionnaires() {
     const [questionnaires, setQuestionnaires] = useState([]);
+    const [gridApi, setGridApi] = React.useState(null);
 
+    
     useEffect(() => fetchData(), []);
+    
 
     const fetchData = () => {
-        fetch('https://api.rss2json.com/v1/api.json?rss_url=https:%2F%2Ffeeds.publicradio.org%2Fpublic_feeds%2Fsong-of-the-day%2Frss%2Frss') // testidata
+        fetch('https://kyselypalvelu.herokuapp.com/api/kyselies') // testidata
         .then(response => response.json())
-        .then(data => setQuestionnaires(data.items)) // vaihdetaan jotain tilalle oikeat jutut
+        .then(data => {
+            
+            console.log("KYSELYN NIMI " + data._embedded.kyselies[0].nimi)    
+            console.log("KYSYMYS 0:N TEKSTI " + data._embedded.kyselies[0].kysymys[0].teksti)
+            setQuestionnaires(data._embedded.kyselies)}
+            ) // vaihdetaan jotain tilalle oikeat jutut    
+
     }
+    
+    // valuegetter, palauttaa kolumnien kysymykset 
+    function kysymysGetter(params){
+        const kysymykset =[params.data.kysymys]
+        let loopdata =''
+        let i = 0;
+        while   ( i < kysymykset[0].length){
+            loopdata +="Kysymys "+ (i+1)+  ": "+  kysymykset[0][i].teksti + " |||||| "
+            i++
+        }
+        return loopdata
+    }
+    // asetuksia
+    const gridOptions ={
+        sortable: true, filter: true,  animateRows: true, resizable: true, wrapText: true, autoHeight: true, 
+        }
+    // on grid load lataa aggrid api, jossa jotain ominaisuuksia 
+    const onGridReady = (params) => {
+            setGridApi(params.api);
+          };
+    //skaalaa gridin ladatessa, pois käytöstä atm
+    const onFirstDataRendered = () => {
+            gridApi.sizeColumnsToFit();
+          };
 
     // fieldit on testidataa, muutetaan myöhemmin oikeaksi
     const columns = [
-        {headerName: "Nimi", field: "title"},
-        {headerName: "Kysymys 1", field: "author"},
-        {headerName: "Kysymys 2", field: "description"},
-        {headerName: "Kysymys 3", field: "enclosure.type"}
+        {headerName: 'Nimi',headerClass:'header', field: 'nimi', width: 250},
+        {headerName: 'Kysymykset',headerClass:'header',  valueGetter:kysymysGetter, width: 1000}
     ]
+   
 
     return (
         <div className="ag-theme-material" style={{height: 600, margin: 'auto'}}>
             <AgGridReact
                 columnDefs={columns}
-                rowData={questionnaires}>
+                defaultColDef={gridOptions}
+                rowData={questionnaires}
+                onGridReady={onGridReady}
+                // onFirstDataRendered = {onFirstDataRendered}
+            
+                >
+                
             </AgGridReact>
         </div>
     );
